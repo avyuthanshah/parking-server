5d8638760c3e88e4ec063446047846f3983fe5b6
Avyu <66016081+avyuthanshah@users.noreply.github.com>
2023-06-09 01:30:54 +0545

    Update server.js

--- server.js
+++ server.js
@@ -224,7 +224,7 @@ app.post("/exit", (req, res) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
